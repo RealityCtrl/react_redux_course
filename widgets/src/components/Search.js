3b7c23cd15dd218071bb0e6ec3e96a70d9f97a92
David McCandless <david.mccandless@outlook.com>
2022-04-02 14:59:46 +0100

    end of video 163

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,20 +2,51 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
+const regex = /(<([^>]+)>)|(&quot;)/gi //NEW
+
 const Search = () => {
 
     const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('to do');
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params:{
+                    action:'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: {searchTerm}
+                }
+            });
+            setResults(data.query.search)
         }
-
-        search();
-
+        if(searchTerm){
+            search();
+        }
+        
         console.log('Used effect2')
 
     }, [searchTerm]);
+
+    const renderedResults = results.map((result) => {
+        
+        const cleanSnippet = result.snippet.replace(regex, "");
+       // <span dangerouslySetInnerHTML={ {__html: result.snippet}}></span>
+        
+        return (
+            <div key={result.pageid} className='item'>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                    
+                    {cleanSnippet}
+                </div>
+            </div>
+        );
+    })
 
     return <div>
             <div className='ui form'>
@@ -28,6 +59,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
     </div>
 };
 
